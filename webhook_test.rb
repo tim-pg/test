c4784cb2794cb966c66e5e885510a1e596e9d3ae
Ubuntu <ubuntu@ip-10-150-94-181.ec2.internal>
2014-08-16 23:23:11 +0000

    add body dump

--- webhook_test.rb
+++ webhook_test.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 before do
   puts '[Params]'
   p params
+  request.body.rewind
+  #request_payload = JSON.parse request.body.read
+  puts '[body]'
+  puts "#{request.body.read}"
 end
 
 get '/hi' do
